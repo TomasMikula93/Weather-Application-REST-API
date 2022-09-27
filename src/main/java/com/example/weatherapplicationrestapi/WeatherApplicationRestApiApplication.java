dcfc8d0d32e88bb2205b1cf0f01bb3e15a439867
TomasMikula <t.mikula@centrum.cz>
2022-09-27 08:49:18 +0200

    project created

---
+++ src/main/java/com/example/weatherapplicationrestapi/WeatherApplicationRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherapplicationrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApplicationRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApplicationRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/weatherapplicationrestapi/WeatherApplicationRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherapplicationrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
